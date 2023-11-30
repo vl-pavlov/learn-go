f59ec76abb870facaf8929ee646309acd4269381
Vladimir Pavlov <pavloff.com@yandex.ru>
2023-11-30 19:54:51 +0500

    standard-library/math: add section

---
+++ 011-standard-library/math/main.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+
+	//--	Basic arithmetic functions:
+	// Abs(x float64) float64: Returns the absolute value of a number
+	// Max(x, y float64) float64: Returns the larger of two numbers
+	// Min(x, y float64) float64: Returns the smaller of two numbers
+	// Mod(x, y float64) float64: Returns the remainder of dividing x by y
+
+	//--	Trigonometric functions:
+	// Sin(x float64) float64: Returns the sine of an angle (in radians)
+	// Cos(x float64) float64: Returns the cosine of an angle (in radians)
+	// Tan(x float64) float64: Returns the tangent of an angle (in radians)
+	// Functions for inverse trigonometric functions (asin, acos, atan, etc.) are also available
+
+	//--	Exponential and logarithmic functions:
+	// Exp(x float64) float64: Returns the base-e exponent of x.
+	// Log(x float64) float64: Returns the natural logarithm (base-e) of x.
+	// Functions for logarithms with other bases (log10, log2) are available as well.
+
+	//--	Rounding and comparison functions:
+	// Floor(x float64) float64: Returns the largest integer less than or equal to x.
+	// Ceil(x float64) float64: Returns the smallest integer greater than or equal to x.
+	// Round(x float64) float64: Rounds x to the nearest integer.
+	// IsNaN(x float64) bool: Checks if x is Not a Number (NaN).
+
+	//--	Other functions:
+	// Sqrt(x float64) float64: Returns the square root of x.
+	// Pow(x, y float64) float64: Returns x raised to the power of y.
+	// The math package also provides functions for advanced mathematical operations like hyperbolic functions, Bessel functions, etc.
+
+	//--	Simple examples
+
+}
