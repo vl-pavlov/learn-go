d69174daa496bf86e18c35e8abf364b2c5cc3ed9
Vladimir Pavlov <pavloff.com@yandex.ru>
2024-02-16 23:09:28 +0500

    web-applications/gin-framework: add section

---
+++ 110-web-applications/gin-framework/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World 👋!")
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
