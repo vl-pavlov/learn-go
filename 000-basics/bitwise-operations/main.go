b9c4e7e511eb8e22d34a8884fcc3140da646521f
Vladimir Pavlov <pavloff.com@yandex.ru>
2024-02-21 20:30:39 +0500

    basics/bitwise-operations: update headers

--- 000-basics/bitwise-operations/main.go
+++ 000-basics/bitwise-operations/main.go
@@ -1,36 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	//--	Bitwise AND (&)
+	//--	BITWISE AND (&)
 
 	resultAnd := 5 & 3
 
 	fmt.Println(resultAnd)
 	// Output: 1 (101 & 011 = 001)
 
-	//--	Bitwise OR (|)
+	//--	BITWISE OR (|)
 
 	resultOr := 5 | 3
 
 	fmt.Println(resultOr)
 	// Output: 7 (101 | 011 = 111)
 
-	//--	Bitwise XOR (^):
+	//--	BITWISE XOR (^)
 
 	resultXor := 5 ^ 3
 
 	fmt.Println(resultXor)
 	// Output: 6 (101 ^ 011 = 110)
 
-	//--	Bitwise Shift Operations (<<, >>):
+	//--	BITWISE SHIFT (<<, >>)
+
 	resultShift := 8 << 1
 	fmt.Println(resultShift)
 	// Output: 16 (01000 << 1 = 10000)
 
 	resultShift = 8 >> 2     // Right shift by 2 positions
 	fmt.Println(resultShift) // Output: 2 (1000 >> 2 = 0010)
-
 }
